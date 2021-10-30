58974901155c0ee56405a24d6284d451fc8dde93
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2021-10-31 00:04:16 +0200

    next

--- CMD/ansible/roles/CMD_BQ666_MY_CONFIG/tasks/update.sh
+++ CMD/ansible/roles/CMD_BQ666_MY_CONFIG/tasks/update.sh
@@ -1,2 +1,2 @@
 #! /usr/bin/env bash
-sed "/become: 'yes'/d" ../../CMD_ROOT_MY_CONFIG_GLOBAL/tasks/main.yml > main.yml
+sed "s/become: 'yes'/become: 'no'/g; s/reg_root_git_pull_config_repo/reg_bq666_git_pull_config_repo/g" ../../CMD_ROOT_MY_CONFIG_GLOBAL/tasks/main.yml > main.yml
