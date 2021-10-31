a207f8810cd9560292ceee2a1b7f93eef6f65f27
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2021-10-31 22:28:16 +0100

    next

--- CMD/ansible/playbooks/moveKDELocalSettingsToRepo.sh
+++ CMD/ansible/playbooks/moveKDELocalSettingsToRepo.sh
@@ -7,6 +7,8 @@ cp ~/.local/share/wallpapers/left.png ~/.my-config/KDE/.local/share/wallpapers/
 cp ~/.local/share/wallpapers/center.jpg ~/.my-config/KDE/.local/share/wallpapers/
 cp ~/.local/share/wallpapers/right.jpg ~/.my-config/KDE/.local/share/wallpapers/
 cp ~/.local/share/wallpapers/top.jpg ~/.my-config/KDE/.local/share/wallpapers/
+cp ~/.config/conky/.conky.conf ~/.my-config/KDE/.config/conky/
 
 # AUTO START
 cp ~/.config/autostart/com.gexperts.tilix.desktop ~/.my-config/KDE/.config/autostart/
+cp ~/.config/autostart-scripts/conky-startup.sh ~/.my-config/KDE/.config/conky/
