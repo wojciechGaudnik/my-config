9134922f3c6b977e174ae3a95553d93dc11eeafd
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2021-10-31 20:22:24 +0100

    next

--- CMD/ansible/playbooks/moveKDELocalSettingsToRepo.sh
+++ CMD/ansible/playbooks/moveKDELocalSettingsToRepo.sh
@@ -2,4 +2,8 @@
 ~/.my-config/KDE/tilix/saveTilixConf.sh
 cp ~/.config/konsolerc ~/.my-config/KDE/.config/
 cp ~/.config/kglobalshortcutsrc ~/.my-config/KDE/.config/
-cp ~/.config/khotkeysrc ~/.my-config/KDE/.config
+cp ~/.config/khotkeysrc ~/.my-config/KDE/.config/
+cp ~/.local/share/wallpapers/left.png ~/.my-config/KDE/.local/share/wallpapers/
+cp ~/.local/share/wallpapers/center.jpg ~/.my-config/KDE/.local/share/wallpapers/
+cp ~/.local/share/wallpapers/right.jpg ~/.my-config/KDE/.local/share/wallpapers/
+cp ~/.local/share/wallpapers/top.jpg ~/.my-config/KDE/.local/share/wallpapers/
