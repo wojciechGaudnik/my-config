3a00ac07c62d6ab7af23c66e6bc4667e946d866a
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2021-10-31 11:56:33 +0100

    next

--- CMD/ansible/roles/CMD_BQ666_MY_CONFIG/tasks/update.sh
+++ CMD/ansible/roles/CMD_BQ666_MY_CONFIG/tasks/update.sh
@@ -1,2 +1,4 @@
 #! /usr/bin/env bash
-sed "s/become: 'yes'/become: 'no'/g; s/reg_root_git_pull_config_repo/reg_bq666_git_pull_config_repo/g" ~/.my-config/CMD/ansible/roles/CMD_ROOT_MY_CONFIG_GLOBAL/tasks/main.yml > ~/.my-config/CMD/ansible/roles/CMD_BQ666_MY_CONFIG/tasks/main.yml
+
+echo "# ---> Don't change this file manually <---"
+sed "s/become: 'yes'/become: 'no'/g; s/reg_root_git_pull_config_repo/reg_bq666_git_pull_config_repo/g" ~/.my-config/CMD/ansible/roles/CMD_ROOT_MY_CONFIG_GLOBAL/tasks/main.yml >> ~/.my-config/CMD/ansible/roles/CMD_BQ666_MY_CONFIG/tasks/main.yml
