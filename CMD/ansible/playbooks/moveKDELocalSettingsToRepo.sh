666c36708839624bae26b05934a214d8f3c3313c
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2021-10-31 18:50:12 +0100

    next

---
+++ CMD/ansible/playbooks/moveKDELocalSettingsToRepo.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+~/.my-config/KDE/tilix/saveTilixConf.sh
+cp ~/.config/konsolerc ~/.my-config/KDE/.config/
+cp ~/.config/kglobalshortcutsrc ~/.my-config/KDE/.config/
+cp ~/.config/khotkeysrc ~/.my-config/KDE/.config
--- CMD/ansible/playbooks/updateKDEConfigs.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-~/.my-config/KDE/tilix/saveTilixConf.sh
