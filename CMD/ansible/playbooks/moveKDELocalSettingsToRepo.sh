4dd84aed0b017603c8464207fee80db66a099600
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2021-11-01 19:34:08 +0100

    next

--- CMD/ansible/playbooks/moveKDELocalSettingsToRepo.sh
+++ CMD/ansible/playbooks/moveKDELocalSettingsToRepo.sh
@@ -13,7 +13,7 @@ cp ~/.config/libreoffice/4/user/registrymodifications.xcu ~/.my-config/KDE/.conf
 cp ~/.config/libreoffice/4/user/basic/Standard/Module1.xba ~/.my-config/KDE/.config/libreoffice/4/user/basic/Standard/
 cp ~/.config/katerc ~/.my-config/KDE/.config/
 cp ~/.config/dolphinrc ~/.my-config/KDE/.config/
-
+cp ~/.local/share/kxmlgui5/dolphin/dolphinui.rc ~/.my-config/KDE/.local/share/kxmlgui5/dolphin/
 # AUTO START
 cp ~/.config/autostart/com.gexperts.tilix.desktop ~/.my-config/KDE/.config/autostart/
 cp ~/.config/autostart-scripts/conky-startup.sh ~/.my-config/KDE/.config/conky/
