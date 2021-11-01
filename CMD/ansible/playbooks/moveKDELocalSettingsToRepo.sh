36b92aebdeab16f1bb3fba57389787170e5dad58
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2021-11-01 17:18:54 +0100

    next

--- CMD/ansible/playbooks/moveKDELocalSettingsToRepo.sh
+++ CMD/ansible/playbooks/moveKDELocalSettingsToRepo.sh
@@ -9,6 +9,10 @@ cp ~/.local/share/wallpapers/right.jpg ~/.my-config/KDE/.local/share/wallpapers/
 cp ~/.local/share/wallpapers/top.jpg ~/.my-config/KDE/.local/share/wallpapers/
 cp ~/.config/conky/.conky.conf ~/.my-config/KDE/.config/conky/
 cp ~/.config/solaar/config.json ~/.my-config/KDE/.config/solaar/
+cp ~/.config/libreoffice/4/user/registrymodifications.xcu ~/.my-config/KDE/.config/libreoffice/4/user/
+cp ~/.config/libreoffice/4/user/basic/Standard/Module1.xba ~/.my-config/KDE/.config/libreoffice/4/user/basic/Standard/
+
 # AUTO START
 cp ~/.config/autostart/com.gexperts.tilix.desktop ~/.my-config/KDE/.config/autostart/
 cp ~/.config/autostart-scripts/conky-startup.sh ~/.my-config/KDE/.config/conky/
+cp ~/.config/autostart/libreoffice-calc.desktop ~/.my-config/KDE/.config/autostart/
