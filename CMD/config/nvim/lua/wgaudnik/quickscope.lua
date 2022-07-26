933ca2118cc825959400b01ca0f0c4c33c3d61bc
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2022-07-26 17:58:43 +0200

    next

--- CMD/config/nvim/lua/wgaudnik/quickscope.lua
+++ CMD/config/nvim/lua/wgaudnik/quickscope.lua
@@ -1,5 +1,5 @@
 vim.g.qs_highlight_on_keys = {'f', 'F', 't', 'T'}
 vim.g.qs_max_chars = 150
-vim.highlight.create('QuickScopePrimary', {guifg='#afff5f', gui=underline, cterm=underline})
-vim.highlight.create('QuickScopeSecondary', {guifg='#00C7DF', gui=underline, cterm=underline})
+vim.highlight.create('QuickScopePrimary', {guifg='#afff5f', gui='underline', cterm='underline'})
+vim.highlight.create('QuickScopeSecondary', {guifg='#00C7DF', gui='underline', cterm='underline'})
 
